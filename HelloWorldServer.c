2143b00beb8d56cbac22bbb581d0642a520669c4
root <root@localhost.localdomain>
2018-10-18 19:02:23 +0900

    나의 첫번째 소스코드

--- HelloWorldServer.c
+++ HelloWorldServer.c
@@ -2,13 +2,9 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <string.h>
-// 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
 #define PORT 9000
-//#define PORT 10000
  
-// 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
-//char buffer[100] = "hello, world\n";
-char buffer[100] = "Hi, I'm server\n";
+char buffer[BUFSIZ] = "Hi, I'm server\n";
  
 main( )
 {
@@ -17,11 +13,10 @@ main( )
 	int   len;
 	int   n;
 	int rcvLen;
-	char rcvBuffer[100];
+	char rcvBuffer[BUFSIZ], *ptr, *ptr2;
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
-	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	s_addr.sin_family = AF_INET;
 	s_addr.sin_port = htons(PORT);
@@ -39,17 +34,46 @@ main( )
 	while(1) {
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
 		printf("Client is connected\n");
 		while(1){
 			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
 			rcvBuffer[rcvLen] = '\0';
 			printf("[%s] received\n", rcvBuffer);
+		
 			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
 				break;
-			n = strlen(buffer);
-			write(c_socket, buffer, n);
+			
+			if(strncmp(rcvBuffer, "안녕하세요", strlen("안녕하세요")) == 0){
+				strcpy(buffer, "안녕하세요. 만나서 반가워요.");
+			} else if(strncmp(rcvBuffer, "이름이 뭐야?", strlen("이름이 뭐야?"))==0){
+				strcpy(buffer, "내 이름은 오승미야");
+			} else if(strncmp(rcvBuffer, "몇 살이야?", strlen("몇 살이야?"))==0){
+				strcpy(buffer, "나는 21살이야");
+			} else{
+				strcpy(buffer, "No data");
+			}
+
+			if(strncmp(rcvBuffer, "strlen ", 7) == 0){
+				n = strlen(rcvBuffer)-7;
+				sprintf(buffer, "%d", n);
+			} else if(strncmp(rcvBuffer, "strcmp ", 7) == 0){
+				ptr = strtok(rcvBuffer, " ");
+
+				ptr = strtok(NULL, " ");
+				ptr2 = strtok(NULL, " ");
+
+				if(strcmp(ptr, ptr2) == 0){
+				    	n = 0;
+					sprintf(buffer, "%d", n);
+				} else {
+					n = -1;
+					sprintf(buffer, "%d", n);
+				}
+			}
+
+			write(c_socket, buffer, strlen(buffer));
 		}
+
 		close(c_socket);
 		if(!strncasecmp(rcvBuffer, "kill server", 11))
 			break;
