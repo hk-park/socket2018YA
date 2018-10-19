f561e270d50457f550648f7d02f9bcf9448e73d3
dongyang <dongyang@localhost.localdomain>
2018-10-19 20:33:12 +0900

    [10.16] 모든 실습 완료

---
+++ ChatBotServer.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+// 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
+#define PORT 9000
+// #define PORT 10000
+  
+// 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
+// char buffer[100] = "hello, world\n";
+char buffer[100] = "Hi, I'm server\n";
+
+main( )
+{
+	int   c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int   len;
+	int   n;
+	int rcvLen;
+	int rcvstrlen;
+	char rcvBuffer[100];
+ 	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&s_addr, 0, sizeof(s_addr));
+	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");   
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+ 
+	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not Bind\n");
+		return -1;
+	}
+	if(listen(s_socket, 5) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+	while(1) {
+		char *compare;
+		char *str[3];
+		int i=0;
+		
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
+		printf("Client is connected\n");
+		while(1){
+			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+			rcvBuffer[rcvLen] = '\0';
+
+		printf("[%s] received\n", rcvBuffer);
+			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
+				break;
+			if(strncasecmp(rcvBuffer,"안녕하세요.", 11) == 0) {
+                               printf("%s 만나서 반가워요.\n", rcvBuffer);
+                                read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+                       }
+                        if(strncasecmp(rcvBuffer, "이름이 뭐야?", 12) == 0) {
+                                printf("내 이름은 노수현이야.\n");
+                                read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+                        }
+                        if(strncasecmp(rcvBuffer,"몇 살이야?", 10) == 0) {
+                                printf("나는 22살이야.\n", rcvBuffer);
+                                read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+                        }
+                        if(strncasecmp(rcvBuffer, "strlen ", 7) == 0) {
+                                rcvstrlen=rcvLen-7;
+                                printf("%d\n", rcvstrlen);
+                                read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+                        }		
+			if(strncasecmp(rcvBuffer, "strcmp ", 7) == 0) {
+				i=0;
+				compare=strtok(rcvBuffer, " ");
+				while (compare != NULL) {
+					str[i++] = compare;
+					compare = strtok(NULL, " ");
+				}
+				if(i<3)	sprintf(buffer, "Please wirte two string.");
+				else if(!strcmp(str[1],str[2]))	sprintf(buffer, "%s and %s are same.", str[1], str[2]);
+				else	sprintf(buffer, "%s and %s are different.", str[1], str[2]);
+			}
+	
+			n = strlen(buffer);
+			write(c_socket, buffer, n);
+		}
+		close(c_socket);
+		if(!strncasecmp(rcvBuffer, "kill server", 11))
+			break;
+
+	}	
+	close(s_socket);
+}
+
+
+
+
+
+
