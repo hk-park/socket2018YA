7fa5f6a3cc1f14bf097f501c66968347cdcef79a
211 <211@localhost.localdomain>
2018-10-23 12:52:01 +0900

    미완

---
+++ fp.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define BUFSIZE 100
+int main(){
+FILE *fp;
+char buffer[BUFSIZE];
+c_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+fp=fopen("test.txt","r");
+
+while(1){
+
+fgets(Buffer, sizeof(Buffer), stdin);
+Buffer[strlen(Buffer)-1] = '\0';
+write(c_socket, Buffer, strlen(Buffer):);
+if()
+
+else if(fp){
+while(fgets(buffer, BUFSIZE, (FILE*)fp));
+printf("%s\n",buffer);
+}
+else if(
+else
+printf("file not found ");
+
+
+
+int result = system("ls > list.txt");
+if(!result)
+printf("명령어가 정상적으로 수행 되었습니다.\n");
+else
+printf("명령어 수행에 실패 했습니다.");
+return 0;
+}
