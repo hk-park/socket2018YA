4edbe0b58d59f9bb6a4ef1d2d74330c7dc700bd6
root <root@localhost.localdomain>
2018-11-20 11:35:00 +0900

    [11.20]fork_test.c 추가

---
+++ fork_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+		int pid;
+		int a = 10;
+		int b = 20;
+
+		a = a+1;
+		pid = fork();
+		//pid = 자식 프로세스의 프로세스 아이디(pid) if 부모 프로세스
+		//pid = 0 if 자식 프로세스
+		//pid = -1 if fork() 함수 실패
+		
+		if(pid >0){//부모 프로세스
+			a = a+1;
+			sleep(30);
+			printf("[PARENT] a = %d, b = %d\n",a,b);
+		}else if(pid == 0){//자식 프로세스
+			b = b+1;
+			sleep(20);
+			printf("[CHILD] a = %d, b = %d\n",a,b);
+		}else{//fork() 실패
+			printf("fork() faild\n");
+		}
+		return 0;
+} 
