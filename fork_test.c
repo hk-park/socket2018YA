a60d681e118a873ba2ca9ff4b6ff3707f2228e26
HanuriKim <albam10@naver.com>
2018-11-20 11:37:47 +0900

    [1120]실습

---
+++ fork_test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	int pid;
+	int a = 10;
+	int b = 20;
+
+	a = a+1;
+
+	pid = fork();
+
+	if(pid>0){
+		a = a+1;
+		sleep(30);
+		printf("PARENT");
+	} else if(pid==0){
+		b = b+1;
+		sleep(20);
+		printf("CHILD");
+	} else {
+		printf("fork() Failed\n");
+	}
+
+	printf("[%d] a = %d / b = %d\n", getpid(), a, b);
+}
