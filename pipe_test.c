8d24b41e3d06fffee3118603798f6f1446c39691
LeeJungKwan <ple2014@naver.com>
2018-11-27 12:42:39 +0900

    [11.27]pipe_test.c 추가

---
+++ pipe_test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFSIZE 100
+
+int main(){
+	int fd[2];
+	char buf[BUFSIZE];
+	int pid;
+
+	if(pipe(fd) < 0 ){ //pipe 생성실패시 프로그램 종료
+		printf("Error Pipe 생성실패\n");
+		exit(0);
+	}
+	pid = fork();
+	if(pid > 0 ){//부모 프로세스
+		memset(buf, 0x00, BUFSIZE);
+		read(fd[0], buf, sizeof(buf));
+		printf("[PARENT] (%s) received.\n",buf);
+	}else if (pid == 0){//자식 프로세스
+		memset(buf, 0x00, BUFSIZE); //buf 를 0으로 초기화
+		sprintf(buf, "[%d] Hello, I'm child.", getpid());
+		//write(c_socket, buf, strlen(buf));
+		write(fd[1], buf, strlen(buf));
+	}else{//생성 실패
+		printf("Error fork() 실패\n");
+	}
+}
