699a61a4594e307004e2c9e1d5f13d9e0dce19ed
hello-hama <211@localhost.localdomain>
2018-11-27 12:42:57 +0900

    [11.27] pipe_test

---
+++ pipe_test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFSIZE 100
+int main(){
+	int fd[2];
+	char buf[BUFSIZE];
+	int pid;
+	
+	if(pipe(fd) < 0){ //pipe 생성, 생성 실패 시, 프로그램 종료
+		printf("[ERROR] Pipe 생성 실패\n");
+		exit(0);
+	}
+
+	pid = fork();
+	if(pid > 0){	//부모프로세스
+		//자식 프로세스가 보낸 메세지를 읽어서 출력
+		memset(buf, 0x00, BUFSIZE);
+		read(fd[0], buf, sizeof(buf));
+		printf("[PARENT] (%s) received.\n", buf);
+	
+	}else if(pid == 0){	//자식프로세스
+		// 부모 프로세스에게 메세지를 전달
+		memset(buf, 0x00, BUFSIZE);
+		sprintf(buf, "[%d] Hello, I'm child.", getpid());
+		write(fd[1], buf, strlen(buf));
+	}else{	//생성 실패
+		printf("[ERROR] fork() 실패\n");
+		exit(0);
+	}
+}
