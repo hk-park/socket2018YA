d8845c3cc1cb3473ef4a995e02117a9066232eef
dongyang <dongyang@localhost.localdomain>
2018-11-27 23:41:13 +0900

    [11.27] 접속 클라이언트 개수 수정

--- MultiProcessServer.c
+++ MultiProcessServer.c
@@ -10,45 +10,42 @@
 #define BUFSIZE 10000
 
 void do_service(int c_socket);
-void sig_handler();
+void sig_handler(int signo);
 
-char sendBuffer[BUFSIZE], rcvBuffer[BUFSIZE];
-int c_socket, s_socket;
-struct sockaddr_in s_addr, c_addr;
-int pid;
-int len, rcvLen;
 int client_count = 0;
-char *token;
 
 int main(){
+	int c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int pid;
+	int len;
 	signal(SIGCHLD, sig_handler);
-  s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 
-  memset(&s_addr, 0, sizeof(s_addr));
-  s_addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP 할당
+	memset(&s_addr, 0, sizeof(s_addr));
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP 할당
 	s_addr.sin_family = AF_INET; //어떤 네트워크 프로토콜을 쓸지
-  s_addr.sin_port = htons(PORT);
+	s_addr.sin_port = htons(PORT);
 
-  if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1){
+	if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1){
 			printf("Can not Bind\n");
  			return -1;
-   }
+	}
 
 	if(listen(s_socket, 5) == -1) {
 			printf("listen Fail\n");
 			return -1; 
-   }
+	}
 
    while(1){
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *)&c_addr, &len);
+		printf("Clinet is connected\n");
+		printf("현재 %d개의 클라이언트가 접속하였습니다.\n",++client_count);
 		pid = fork();
 		if(pid > 0){ //부모 프로세스
 			//다른 클라이언트의 요청 접수
-			printf("Clinet is connected\n");
-			printf("현재 %d개의 클라이언트가 접속하였습니다.\n",++client_count);
 			close(c_socket);
-			continue;
 		}
 		else if(pid == 0){ //자식 프로세스
 			//클라이언트의 요청 처리
@@ -62,18 +59,20 @@ int main(){
 			exit(0);
 		}
 	}
+	close(s_socket);
 }
 
 void do_service(int c_socket){
+	char sendBuffer[BUFSIZE], rcvBuffer[BUFSIZE];
+	int rcvLen;
 	while(1){
+		char *token;
 		rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+		rcvBuffer[rcvLen] = '\0';
 		printf("Received Data From Client : %s\n", rcvBuffer);
+
 		if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
 			break;
-		if(rcvLen < 0)
-				exit(0); //do_service()함수를 실행함. read()함수에서 클라이언트가 메시지를 보낼때까지 대기
-		rcvBuffer[rcvLen] = '\0';
-		
 		if(strncasecmp(rcvBuffer, "안녕하세요.", strlen("안녕하세요.")) == 0)
 			strcpy(sendBuffer, "안녕하세요. 만나서 반가워요.\n");
 		else if(strncasecmp(rcvBuffer, "이름이 머야?", strlen("이름이 머야?")) == 0)
@@ -134,10 +133,10 @@ void do_service(int c_socket){
 				sprintf(sendBuffer, "[%s] command is executed\n",command);
 			else
 				sprintf(sendBuffer, "[%s] command failed\n",command);
-		}
+		}else
+			strcpy(sendBuffer, "무슨 말인지 모르겠습니다.");
 		write(c_socket, sendBuffer, strlen(sendBuffer));
 	}
-	close(s_socket);
 }
 
 void sig_handler(int signo){
