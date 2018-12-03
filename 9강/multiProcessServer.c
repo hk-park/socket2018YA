228f482f6e145714b38de7433afcc8b09d5d9314
ajh1000 <moulawola@gmail.com>
2018-12-03 23:19:12 +0900

    kill server 구현 완료

--- 9강/multiProcessServer.c
+++ 9강/multiProcessServer.c
@@ -11,6 +11,9 @@
 //DEFINEs
 #define PORT 9000
 #define BUF_SIZ 100
+#define SIG_KILL_SERVER SIGUSR1
+#define SIG_PRINT SIGUSR2
+#define CLIENT_NUM 5
 
 //GLOBALs
 char rcvBuffer[BUF_SIZ];
@@ -19,22 +22,45 @@ char buffer[BUF_SIZ] = "My name is Hongkyu.\n";
 char fromStr[BUF_SIZ]="Hi, i'm Client\n";
 char toStr[BUF_SIZ]="Hi, i'm Server\n";
 
-int g_pid;
 int g_clientCnt;
+int g_fd[2];
+int g_ppid=0;
+
+typedef struct tag_client_socket_info{
+    int socket;
+    int pid;
+}client_socket_info;
+client_socket_info g_client_sockets[5];
+
+int s_socket;
+
 
 void do_service(int c_socket);
-void sig_handler(int signum);
+void sig_child(int signum);
+void sig_kill_server(int signum);
+void sig_print(int signum);
 
 main( )
 {
-    int   c_socket, s_socket;
+    //s_socket은 전역 영역으로 보냄.
+    int   c_socket;
     struct sockaddr_in s_addr, c_addr;
     int   len;
 
-    signal(SIGCHLD,sig_handler);
+    int pid;
+    int tmp=0;
+    int i=0;
+    
+    signal(SIGCHLD,sig_child);
+    signal(SIG_KILL_SERVER, sig_kill_server);
+    signal(SIG_PRINT,sig_print);
+
+    pipe(g_fd);
+
     s_socket = socket(PF_INET, SOCK_STREAM, 0);
 
     //init socket
+    memset(g_client_sockets,-1,sizeof(client_socket_info)*5);
     memset(&s_addr, 0, sizeof(s_addr));
     s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -46,7 +72,7 @@ main( )
 	return -1;
     }
 
-    if(listen(s_socket, 5) == -1) {
+    if(listen(s_socket, CLIENT_NUM) == -1) {
 	printf("listen Fail\n");
 	return -1;
     }
@@ -56,40 +82,120 @@ main( )
 	c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
 
 	printf("client connected\n");
-	g_pid=fork();
+	pid=fork();
 
-	if(g_pid>0)
+	if(pid>0)
 	{
+	    //add client socket to array.
+	    for(i=0;i<CLIENT_NUM;++i)
+	    {
+		if(g_client_sockets[i].socket == -1 
+			&& g_client_sockets[i].pid == -1)
+		{
+		    g_client_sockets[i].socket=c_socket;
+		    g_client_sockets[i].pid=pid;
+		    break;
+		}
+	    }
+
 	    printf("현재 %d개의 클라이언트가 접속하였습니다.\n",++g_clientCnt);
+	    
+	    //don't need this client socket for parent process.
 	    close(c_socket);
+
+	    //send ppid to child.
+	    g_ppid=getpid();
+	    write(g_fd[1],&g_ppid,sizeof(int));
 	    continue;
 	}
-	else if(g_pid==0)
-	{	
+	else if(pid==0)
+	{
+	    //read ppid from pipe before proceeds service.
+	    read(g_fd[0],&g_ppid,sizeof(int));
+	    
+	    //don't need this server socket copyed from parent process.
 	    close(s_socket);
+
+	    //do service.
+	    //while loop
 	    do_service(c_socket);
+
+	    //quit.
 	    exit(0);
 	}
     }
     close(s_socket);
 }
 
-void sig_handler(int signum)
+void sig_child(int signum)
 {
     int pid;
     int status;
-    pid=wait(&status);
-    
-    //status가 -1이면 system 함수에 의해서 생성된 자식 프로세스가 종료되었다는 의미이다.
-    //우리가 원하는 것은 system 함수에 의해서 발생된 SIGCHLD가 아니라 accept 함수를 통해서
-    //생성된 자식 프로세스의 SIGCHLD이다. status 값이 -1이 아닐때에만 처리하면 어느정도
-    //예외처리는 될것같다.
+    int i=0;
+    pid=wait(&status); 
 
-    if( status != -1)
+    //pid가 -1이면 system 함수에 의해서 생성된 자식 프로세스가 종료되었다는 의미이다.
+    //우리가 원하는 것은 system 함수에 의해서 발생된 SIGCHLD가 아니라 accept 함수를 통해서
+    //생성된 자식 프로세스의 SIGCHLD이다. pid 값이 -1이 아닐때에만 처리한다.
+
+    if( pid != -1)
     {
+	for(i=0;i<CLIENT_NUM;++i)
+	{
+	    if(g_client_sockets[i].pid==pid)
+	    {
+		g_client_sockets[i].socket=-1;
+		g_client_sockets[i].pid=-1;
+		break;
+	    }
+	}
+
 	printf("pid [%d] process terminated. status = %d\n",pid,status);
 	printf("1개의 클라이언트가 접속종료되어 %d개의 클라이언트가 접속되어 있습니다.\n",--g_clientCnt);
     }
+}
+
+void sig_kill_server(int signum)
+{
+    int i=0;
+    int cnt=0;
+    int pid,status;
+    printf("kill server\n");
+   
+    //close all clients
+    for(i=0;i<CLIENT_NUM;++i)
+    {
+	if(g_client_sockets[i].socket !=-1 
+		&& g_client_sockets[i].pid != -1)
+	{
+	    close(g_client_sockets[i]);
+	    printf("%d\n",g_client_sockets[i].pid);
+
+	    //SIGKILL or SIGTERM can kill child process safely.
+	    kill(g_client_sockets[i].pid,SIGTERM);
+	    cnt++;
+	}
+    }
+    
+    printf("총 %d 개의 클라이언트 종료됨.\n",cnt);
+
+    //finally close server
+    close(s_socket);
+    exit(0);
+}   
+
+void sig_print(int signum)
+{
+    int i=0;
+
+    for(i=0;i<CLIENT_NUM;++i)
+    {
+	puts("=================");
+	printf("[%d]\n",i);
+	printf("socket: %d\n",g_client_sockets[i].socket);
+	printf("pid: %d\n",g_client_sockets[i].pid);
+    } 
+    puts("=================");
 }
 
 void do_service(int c_socket)
@@ -130,7 +236,14 @@ void do_service(int c_socket)
 
 	printf("Client Msg : %s",rcvBuffer);
 
-	//process
+	/*
+		!!!CRITICAL BUG POINT!!!
+		g_ppid 를 0으로 초기화 하지 않으면 위의 printf 문을 수행하자마자 g_ppid 값이 변해버림. 알 수 없는 버그.
+	*/
+	//printf("3 %d\n",g_ppid);
+	//printf("4 %d\n",getppid());
+
+	//services
 	if(soc_msgcmp(rcvBuffer,"안녕하세요."))
 	{
 	    soc_write(c_socket,"안녕하세요.만나서 반가워요.");
@@ -211,13 +324,25 @@ void do_service(int c_socket)
 	    close(c_socket);
 	    break;
 	}
-	/*else if(soc_msgcmp(rcvBuffer,"kill server"))
-	  {
-	  soc_write(c_socket,"kill server commited");
-	  close(c_socket);
-	  close(s_socket);
-	  return 0;
-	  }*/
+	else if(soc_msgcmp(rcvBuffer,"kill server"))
+	{
+	    //자식 프로세스에서 raise를 쓰면 자식 프로세스에서만 시그널 발동할 것이기 때문에
+	    //kill 함수를 써서 직접 해당 프로세스에게 시그널을 발생시키도록 했다.
+	    //raise(SIG_KILL_SERVER);
+
+	    soc_write(c_socket,"kill server commited");
+	    //간단하게 getppid()함수를 써도 되는데 배운 pipe()함수를 활용해보기 위해서
+	    //pipe를 통해 부모 프로세스값을 주고 받았다.
+	    kill(g_ppid,SIG_KILL_SERVER);
+
+	    //close(c_socket); 
+	    //break;
+	}
+	else if(soc_msgcmp(rcvBuffer,"print socket"))
+	{
+	    soc_write(c_socket,"printed socket info to server");
+	    kill(g_ppid,SIG_PRINT);
+	}
 	else
 	{
 	    //echo every msg
