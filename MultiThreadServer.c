f681ad012757bc185ab1b567e01299f13148ecb7
211 <211@localhost.localdomain>
2018-12-04 12:47:24 +0900

    MultiThreadServer

---
+++ MultiThreadServer.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+// 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
+#define PORT 9000
+//#define PORT 10000
+#define BUFSIZE 100
+#define SLEEPTIME 1
+ 
+// 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
+//char buffer[100] = "hello, world\n";
+char buffer[BUFSIZE] = "Hi, I'm server\n";
+char buffer2[BUFSIZE];
+char *token;
+char txt[BUFSIZE];
+char str1[BUFSIZE];
+char str2[BUFSIZE];
+FILE *fp;
+char buff[255];
+char rcvBuffer[BUFSIZE];
+
+
+main( )
+{
+	int   c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int   len;
+	int   n;
+	int rcvLen;
+	int readsize;
+	char rcvBuffer[100];
+
+	pthread_t pthread;
+	int thr_id;
+ 	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&s_addr, 0, sizeof(s_addr));
+	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+ 
+	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not Bind\n");
+		return -1;
+	}
+ 
+	if(listen(s_socket, 5) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+ 	
+	while(1) {
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
+		thr_id = pthread_create(&pthread,NULL,do_service,(void*)&len);
+		}
+}
+void *do_servic(void *data)
+{
+	int n;
+	char rcvBuffer[BUFSIZ];
+	int c_socket=*((int *)data);
+
+	while(1){
+		if((readSize = read(c_socket,rcvBuffer,sizeof(rcvBuffer))<0);
+	}
+}
