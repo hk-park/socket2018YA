67a159569597db9d5d7c11e6f90a5a435e79c358
211 <211@localhost.localdomain>
2018-12-04 11:15:57 +0900

    1204 pthread_create 예제 추가

---
+++ pthread_test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *func_thread(void *); //스레드가 수행할 함수
+
+main(){
+	int status;
+	pthread_t thread_id;
+	status = pthread_create(&thread_id, NULL, func_thread, NULL);
+	if(status != 0){
+		printf("[ERROR] thread create fail. status = %d\n", status);
+		exit(1);
+	}
+	printf("thread %x is created. \n", thread_id);
+	pthread_join(thread_id, (void **)&status);
+	printf("main function end.\n");
+
+}
+
+void *func_thread(void * argv){
+	int i = 0;
+	while(i<10){
+		i++;
+		printf("thread %dth executing..\n", i);
+		sleep(1);
+	}
+}
+
+
