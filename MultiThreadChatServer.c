37b070b5f7d383f230754990672ad9c1a7bec646
dongyang <dongyang@localhost.localdomain>
2018-12-12 22:02:54 +0900

    채팅방 구현 완료(귓속말기능까지)

--- MultiThreadChatServer.c
+++ MultiThreadChatServer.c
@@ -11,14 +11,19 @@ int popClient(int); //클라이언트가 종료했을 때 클라이언트 정보
 pthread_t thread;
 pthread_mutex_t mutex;
 #define MAX_CLIENT 10
+#define NAMELEN 20
 #define CHATDATA 1024
 #define INVALID_SOCK -1
 #define PORT 9000
-int list_c[MAX_CLIENT]; //접속한 클라이언트를 관리하는 배열
-int client_num = 0;
+
+typedef struct user_info{
+	int socket;
+	char nickname[NAMELEN]; //접속한 클라이언트의 닉네임
+}client_info;
 char escape[ ] = "exit";
 char greeting[ ] = "Welcome to chatting room\n";
 char CODE200[ ] = "Sorry No More Connection\n";
+client_info client[MAX_CLIENT]; //접속한 클라이언트를 관리하는 배열
 
 int main(int argc, char *argv[ ])
 {
@@ -46,12 +51,21 @@ int main(int argc, char *argv[ ])
     }
     pthread_mutex_lock(&mutex);
     for(i = 0; i < MAX_CLIENT; i++)
-        list_c[i] = INVALID_SOCK;
+        client[i].socket = INVALID_SOCK;
 	pthread_mutex_unlock(&mutex);
     while(1) {
         len = sizeof(c_addr);
         c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
         res = pushClient(c_socket); //접속한 클라이언트를 list_c에 추가
+		for(i=0; i<MAX_CLIENT; i++){
+			pthread_mutex_lock(&mutex);
+			if(client[i].socket == c_socket){
+				read(c_socket, client[i].nickname, sizeof(client[i].nickname));				
+				pthread_mutex_unlock(&mutex);
+				break;
+			}
+			pthread_mutex_unlock(&mutex);
+		}
         if(res < 0) { //MAX_CLIENT만큼 이미 클라이언트가 접속해 있다면,
             write(c_socket, CODE200, strlen(CODE200));
             close(c_socket);
@@ -66,54 +80,64 @@ void *do_chat(void *arg)
 {
     int c_socket = *((int *)arg);
     char chatData[CHATDATA];
-    int i, n, j;
+	char whisper[CHATDATA];
+    int i, n;
 	char *token;
 	char *str[3];
+	
     while(1) {
         memset(chatData, 0, sizeof(chatData));
         if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
             //write chatData to all clients
-						chatData[n] = '\0';
-			if(!strncasecmp(chatData, "/w ", strlen("/w "))){
-				j=0;
-				token = strtok(chatData, " ");
-				while(token != NULL){
-					str[j++] = token;
-					token = strtok(NULL, " ");
-				}
-				strcpy(chatData, str[2]);
-				pthread_mutex_lock(&mutex);
+			chatData[n] = '\0';
+			strcpy(whisper, chatData);
+			token = strtok(whisper, " ");
+			token = strtok(NULL, "\0");
+			strcpy(whisper, token);
+			if(strncasecmp(whisper, "/w", strlen("/w")) == 0){
+				token = strtok(whisper, " "); // token = "/w"
+				str[1] = strtok(NULL, " "); //str[1] = "귓속말 대상"
+				str[2] = strtok(NULL, "\0"); //str[2] = "귓속말 내용"
 				for(i=0; i<MAX_CLIENT; i++){
-					if(strcmp(str[1], list_c[i]))
-						write(list_c[i], chatData, strlen(chatData));
+					pthread_mutex_lock(&mutex);
+					if(!strcmp(str[1], client[i].nickname)){
+						sprintf(chatData, "[%s] %s", str[1], str[2]);
+						write(client[i].socket, chatData, strlen(chatData));
+						pthread_mutex_unlock(&mutex);
+						break;
+					}
+					pthread_mutex_unlock(&mutex);
 				} 
 			}
-			for(i=0; i<MAX_CLIENT; i++){
-				write(list_c[i], chatData, strlen(chatData));
+			else{
+				for(i=0; i<MAX_CLIENT; i++){
+				if(client[i].socket != INVALID_SOCK)
+					write(client[i].socket, chatData, n);
+				}
 			}
 			if(strstr(chatData, escape) != NULL) {
 				popClient(c_socket);
 				break;
            		 }
-        }
-    }
+        	}
+    	}
 }
 int pushClient(int c_socket) {
 	int i=0;
-	if(client_num >= MAX_CLIENT){
-		close(c_socket);
-		return -1;
-	}
-	pthread_mutex_lock(&mutex);
+	
+	
 	for(i = 0; i < MAX_CLIENT; i++){
-		if(list_c[i] == INVALID_SOCK){
-			list_c[i] = c_socket;
-			break;
+		pthread_mutex_lock(&mutex);
+		if(client[i].socket == INVALID_SOCK){
+			client[i].socket = c_socket;
+			pthread_mutex_unlock(&mutex);
+			return i;
 		}
+		pthread_mutex_unlock(&mutex);
 	}
-	client_num++;
-	pthread_mutex_unlock(&mutex);
-	return i;
+	
+	if(i == MAX_CLIENT)
+		return -1;
     //ADD c_socket to list_c array.
     //return -1, if list_c is full.
     //return the index of list_c which c_socket is added.
@@ -121,19 +145,15 @@ int pushClient(int c_socket) {
 int popClient(int c_socket)
 {
 	int i=0;
-	if(client_num < 0)
-		return;
 	pthread_mutex_lock(&mutex);
 	for(i = 0; i < MAX_CLIENT; i++){
-		if(list_c[i] == INVALID_SOCK){
-			list_c[i-1] = INVALID_SOCK;
+		if(c_socket == client[i].socket){
+			client[i].socket = INVALID_SOCK;
 			break;
 		}
 	}
-	if(i == MAX_CLIENT-1)
-		list_c[i] = INVALID_SOCK;
-	client_num--;
 	pthread_mutex_unlock(&mutex);
 	close(c_socket);
+	return 0;
     //REMOVE c_socket from list_c array.	
 }
