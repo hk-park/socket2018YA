906d5e09f05f656b8914d8314abc39fe683f0730
root <root@localhost.localdomain>
2018-12-12 11:40:08 +0900

    MultiThreadChatServer

---
+++ MultiThreadChatServer.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+void *do_chat(void *);
+int pushClient(int);
+int popClient(int);
+pthread_t thread;
+pthread_mutex_t mutex;
+
+#define MAX_CLIENT 10
+#define CHATDATA 1024
+#define INVALID_SOCK -1
+#define PORT 9000
+
+int	list_c[MAX_CLIENT];
+char	escape[ ] = "exit";
+char	greeting[ ] = "Welcome to chatting room\n";
+char	CODE200[ ] = "Sorry No More Connection\n";
+
+int max=0;
+
+int main(int argc, char *argv[ ])
+{
+
+	int c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int	len;
+	int	i, j, n;
+	int	res;
+	if(pthread_mutex_init(&mutex, NULL) != 0) {
+		printf("Can not create mutex\n");
+		return -1;
+	}
+	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&s_addr, 0, sizeof(s_addr));
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+	if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not Bind\n");
+		return -1;
+	}
+
+	if(listen(s_socket, MAX_CLIENT) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+	for(i = 0; i < MAX_CLIENT; i++)
+		list_c[i] = INVALID_SOCK;
+	while(1) {
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		res = pushClient(c_socket);
+		if(res < 0) {
+			write(c_socket, CODE200, strlen(CODE200));
+			close(c_socket);
+		} else {
+			write(c_socket, greeting, strlen(greeting));
+			//pthread_create with do_chat function.
+			pthread_create(&thread,NULL,do_chat, (void*)&c_socket);
+		}
+	}
+}
+
+void *do_chat(void *arg)
+{
+	int c_socket = *((int *)arg);
+	char chatData[CHATDATA];
+	int i, n;
+	while(1) {
+		memset(chatData, 0, sizeof(chatData));
+		if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
+			//write chatData to all clients
+			//
+			///////////////////////////////
+            
+			  int i=0;
+                        for(i=0;i<sizeof(list_c);i++)
+                        {
+                                if(list_c[i]!=-1)
+                                {
+
+                                write(list_c[i],chatData,sizeof(chatData));
+                                }
+   }
+                        if(strstr(chatData, escape) != NULL) {
+
+  			popClient(c_socket);
+                                break;
+                        }
+                }
+        }
+}
+int pushClient(int c_socket) {
+	//ADD c_socket to list_c array.
+	//
+	///////////////////////////////
+	//return -1, if list_c is full.
+	//return the index of list_c which c_socket is added.
+	
+	int i=0;
+        if(max==9)
+        {
+                printf("no more data!\n");
+ 	return -1;
+        }
+        for(i=0;i<sizeof(list_c);i++)
+  {
+
+                if(list_c[i]==-1)
+                {
+                        pthread_mutex_lock(&mutex);
+                        list_c[i]=c_socket;
+                        max++;
+                        pthread_mutex_unlock(&mutex);
+                        break;
+
+                }
+
+
+        }
+
+}
+
+int popClient(int c_socket)
+{			
+	close(c_socket); 
+	//REMOVE c_socket from list_c array.
+	//
+	///////////////////////////////////
+	if(max<0)
+        {
+        printf("can not pop\n");
+        return;
+        }
+        int i=0;
+        for(i=0;i<sizeof(list_c);i++)
+        {
+                if(list_c[i]==c_socket)
+                {
+                        list_c[i]=-1;
+
+                        max--;
+                        break;
+                }
+   }
+        //process mutex ***
+
+}
